09baef4e39cee6962d85dc8193fcdc030aafe150
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-08-14 10:43:48 +0530

    Add files via upload

---
+++ Fundamentals/C2 - Operators/operation.js
@@ -0,0 +1,34 @@
+
+
+function add(x,y){
+    return x+y
+}
+
+function subtract(x,y){
+    return x-y
+}
+
+function  multiply(x,y){
+    return x * y
+}
+
+function division(x,y){
+    return x/y;
+}
+
+function increment(x){
+    return x++;
+}
+
+let a = 90
+a++;
+console.log(a)
+let b = 21
+b--;
+console.log(b)
+
+console.log(add(4,9))
+console.log(subtract(5,8))
+console.log(multiply(12,4))
+console.log(division(23,33))
+console.log(increment(10))
