996199b48753e6385ce3a4318dc23d9f9673d2c8
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-09-24 20:40:55 +0530

    Create parse.js

---
+++ JSON/parse.js
@@ -0,0 +1,7 @@
+<script>  
+//JavaScript to illustrate JSON.stringify() method.  
+var a = [ "JAVA", "C", "C++", "Python" ];  
+var Json = JSON.stringify(a);  
+// expected output: ["JAVA","C","C++","Python"]  
+document.write(Json);  
+</script>  
