3e7b4cecbf060a0c15ca7e2df6df8e2681384cbb
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-11 19:59:29 +0530

    Add files via upload

---
+++ Fundamentals/C6 - Functions/function.js
@@ -0,0 +1,18 @@
+
+
+<html>
+    <head>
+        <title>Javascript Functions</title>
+    </head>
+    <body bgcolor = "red">
+        <h1><center><b>Javascript Functions</b></center></h1>
+        <p id = "demo">JavaScript functions are defined with the function keyword.You can use a function declaration or a function expression.</p>
+        <script>
+
+            function myfunction(a,b){
+                return arguments.length
+            }
+
+        </script>
+    </body>
+</html>
