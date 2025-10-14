afff1a3e30ced00f4d6c1413a727753f5e03dd3e
Adarsh Achuthan <160232793+achuthanadarsh977@users.noreply.github.com>
2025-10-14 19:31:48 +0530

    Create script3.js

---
+++ Projects/Build Notes App/script3.js
@@ -0,0 +1,13 @@
+
+const noteecontainer = document.querySelector(".notes-contianer")
+const createbtn = document.querySelector(".btn")
+
+let notes = document.querySelector(".btn")
+
+
+
+createBtn.addEventListener("click"  , ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    
+})
